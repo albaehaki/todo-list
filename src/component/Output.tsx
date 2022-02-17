5d5248d6c208c539e790560d3004dae2dab55218
alzdirty <zackyalbaehaki@gmail.com>
2022-02-17 21:45:34 +0700

    sama

--- src/component/Output.tsx
+++ src/component/Output.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Output({ todo }: any) {
+  const [radio, setRadio] = useState<string>("");
+
   return (
     <div className="grid">
       <div className="m-auto w-screen md:w-1/2 grid">
-        {todo.map((todo: any) => {
-          return <p className="px-2">{todo}</p>;
+        {todo.map((todo: any, i: number) => {
+          return (
+            <div key={i} className="flex px-2 flex-row">
+              <input
+                className="my-auto"
+                value={radio}
+                onClick={(e: any) => {
+                  console.log(e.target.value);
+                }}
+                type="checkbox"
+              />
+              <p className="px-2">{todo}</p>
+            </div>
+          );
         })}
       </div>
     </div>
   );
 }
+//<p className="px-2">{todo}</p>
 
 export default Output;
