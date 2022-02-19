fd10fbd3587039a4340157464c0893f0fe7af0d8
alzdirty <zackyalbaehaki@gmail.com>
2022-02-19 10:09:49 +0700

    wah sekarang dah bisa menampilkan kegiatan yang dah selesai

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   const [text, setText] = useState<string>("");
   const [ToDo, setToDo] = useState<any>([]);
   const [done, setDone] = useState<any>([]);
-  const [key, setKey] = useState<any>();
+  const [stasiun, setStasiun] = useState<any>([]);
 
   const onchange = (e: any) => {
     setText(e.target.value);
@@ -25,9 +25,14 @@ function App() {
   const selesai = (e: any) => {
     if (e.target.checked) {
       console.log(ToDo[e.target.value]);
-      console.log(ToDo[e.target.value].id);
-      console.log(ToDo.filter((a: any) => a !== a[e.target.value]));
-      setDone(ToDo.filter((a: any) => a.includes(a[e.target.value])));
+
+      setDone(done.concat(ToDo.filter((a: any) => a == ToDo[e.target.value])));
+
+      setToDo(ToDo.filter((a: any) => a !== ToDo[e.target.value]));
+
+      if (e.target.checked) {
+        e.target.checked = false;
+      }
     } else {
     }
   };
@@ -36,7 +41,7 @@ function App() {
     <div className="grid text-gray-500 font-mono">
       <p className="m-auto my-5">apa yang ingin anda lakukan hari ini...</p>
       <Input state={text} onchange={onchange} onsubmit={onsubmit} />
-      <Output todo={ToDo} hapus={selesai} setKey={setKey} />
+      <Output todo={ToDo} done={done} hapus={selesai} />
     </div>
   );
 }
--- src/component/Output.tsx
+++ src/component/Output.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Output({ todo, hapus, setKey }: any) {
+function Output({ todo, hapus, done }: any) {
   const [radio, setRadio] = useState<string>("");
 
   return (
@@ -12,13 +12,28 @@ function Output({ todo, hapus, setKey }: any) {
               <input
                 className="my-auto accent-gray-500"
                 onChange={(e: any) => {
-                  setKey(i);
                   hapus(e);
                 }}
                 value={i}
                 type="checkbox"
               />
               <p className="px-2 break-all">{todo}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div className="m-auto w-10/12 sm:w-8/12 md:w-1/2 grid">
+        <p className="px-2 py-1">selesai</p>
+        {done.map((todo: any, i: number) => {
+          return (
+            <div key={i} className="flex px-2 border-b-[1px] py-1 flex-row">
+              <input
+                className="my-auto accent-gray-500"
+                value={i}
+                checked
+                type="checkbox"
+              />
+              <p className="px-2 break-all line-through">{todo}</p>
             </div>
           );
         })}
